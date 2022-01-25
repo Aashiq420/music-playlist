961c0998f38b3db796c9084c4e059ed23fbe9700
Aashiq420 <adams.aashiq@gmail.com>
2022-01-25 11:51:51 +0200

    Login/signup + firebase config

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,22 @@
 <template>
-<div class="content">
-  <router-view/>
-</div>
+  <Navbar />
+  <div class="content">
+    <router-view/>
+  </div>
 </template>
 
-<style>
-.content {
-  margin: 0 auto;
-  max-width: 1200px;
-  padding: 0 20px;
+<script>
+import Navbar from './components/Navbar.vue'
+
+export default {
+  components: { Navbar }  
 }
+</script>
+
+<style>
+  .content {
+    margin: 0 auto;
+    max-width: 1200px;
+    padding: 0 20px;
+  }
 </style>
